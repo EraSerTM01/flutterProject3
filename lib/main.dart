bea5da49e4644a5fc4c83008bf89ce7457f1bbb7
Oleksandr Khvostov <fennomcold@gmail.com>
2024-03-14 13:05:37 +0200

    feat: create app with container, column, row, expanded; use color, size, shadow, background and  crossAxisAlignment, mainAxisAlignment

---
+++ lib/main.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Facebook Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Facebook', style: TextStyle(color: Colors.blue)),
+        backgroundColor: Colors.white,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.blue),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.message, color: Colors.blue),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Scrollbar(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Container(
+                color: Colors.grey[300],
+                padding: const EdgeInsets.all(10.0),
+                child: const Text(
+                  'News Feed',
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            const PostWidget(
+              avatarImage: 'assets/user_avatar.jpg',
+              userName: 'Oleksandr Khvostov',
+              postTime: '2 hours ago',
+              postText: 'Cute puppy',
+              imagePath: 'assets/cute-little-puppy.jpg',
+            ),
+            const PostWidget(
+              avatarImage: 'assets/mrRobotsAvatar.jpg',
+              userName: 'MrRobot',
+              postTime: '3 hours ago',
+              postText: 'A little bit about Meta',
+              imagePath: 'assets/metaPost.jpg',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PostWidget extends StatelessWidget {
+  final String avatarImage;
+  final String userName;
+  final String postTime;
+  final String postText;
+  final String imagePath;
+
+  const PostWidget({
+    Key? key,
+    required this.avatarImage,
+    required this.userName,
+    required this.postTime,
+    required this.postText,
+    required this.imagePath,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10.0),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 20.0,
+                  backgroundImage: AssetImage(avatarImage),
+                ),
+                SizedBox(width: 10.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(userName,
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(postTime, style: TextStyle(color: Colors.grey)),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 10.0),
+            Text(
+              postText,
+              style: TextStyle(fontSize: 16.0),
+            ),
+            SizedBox(height: 10.0),
+            Image.asset(imagePath),
+            SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.thumb_up),
+                        onPressed: () {},
+                      ),
+                      const Text('Like'),
+                      const SizedBox(width: 8),
+                      IconButton(
+                        icon: const Icon(Icons.comment),
+                        onPressed: () {},
+                      ),
+                      const Text('Comment'),
+                      const SizedBox(width: 8),
+                      IconButton(
+                        icon: Icon(Icons.share),
+                        onPressed: () {},
+                      ),
+                      const Text('Share'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
